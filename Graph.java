b8aa4afdc14bbfddbf24386cb1285aed1fabe097
Jonathan Moses <jon.moses2000@gmail.com>
2026-06-24 15:37:43 -0600

    file set up and beginning work

--- Graph.java
+++ Graph.java
@@ -1,5 +1,30 @@
 package assign07;
 
-public class Graph {
+import java.util.List;
 
+public class Graph<T> {
+	private Vertex[] vertices;
+	
+	
+	public boolean depthFirstSearch(Vertex source, Vertex destination) {
+		
+	}
+	
+	public List<Vertex> breadthFirstSearch(Vertex source, Vertex destination) {
+		
+	}
+
+	
+	public List<Vertex> topoSort() {
+		
+	}
+	
+	private class Vertex {
+		private T data;
+		
+		
+		public Vertex(T data) {
+			this.data = data;
+		}
+	}
 }
--- GraphUtility.java
+++ GraphUtility.java
@@ -1,5 +1,15 @@
 package assign07;
 
 public class GraphUtility {
-
+	public static <Type> boolean areConnected(List<Type> sources, List<Type> destinations, Type srcData, Type dstData) {
+		
+	}
+	
+	public static <Type> List<Type> shortestPath(List<Type> sources, List<Type> destinations, Type srcData, Type dstData) {
+		
+	}
+	
+	public static <Type> List<Type> sort(List<Type> sources, List<Type> destinations) {
+		
+	}
 }
