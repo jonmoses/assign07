2fb6c73028f420ff92a8be21ed2b56ce6108a38a
JaronBurton <jaron.burton@icloud.com>
2026-06-25 09:37:38 -0600

    added throws

--- GraphUtility.java
+++ GraphUtility.java
@@ -5,6 +5,14 @@ import java.util.List;
 public class GraphUtility {
 
 	public static <Type> boolean areConnected(List<Type> sources, List<Type> destinations, Type srcData, Type dstData) {
+		if (sources == null || destinations == null) {
+			throw new IllegalArgumentException("Source and destination lists cannot be null.");
+		}
+
+		if (sources.size() != destinations.size()) {
+			throw new IllegalArgumentException("Source and destination lists must be the same size.");
+		}
+
 		Graph<Type> graph = new Graph<Type>();
 		for (int i = 0; i < sources.size(); i++) {
 			Type src = sources.get(i);
@@ -15,9 +23,16 @@ public class GraphUtility {
 		}
 		return graph.depthFirstSearch(srcData, dstData);
 	}
-	
-	public static <Type> List<Type> shortestPath(List<Type> sources, List<Type> destinations, Type srcData, Type dstData) {
-		
+
+	public static <Type> List<Type> shortestPath(List<Type> sources, List<Type> destinations, Type srcData,
+			Type dstData) {
+		if (sources == null || destinations == null) {
+			throw new IllegalArgumentException("Source and destination lists cannot be null.");
+		}
+
+		if (sources.size() != destinations.size()) {
+			throw new IllegalArgumentException("Source and destination lists must be the same size.");
+		}
 		Graph<Type> graph = new Graph<Type>();
 		for (int i = 0; i < sources.size(); i++) {
 			Type src = sources.get(i);
@@ -26,11 +41,18 @@ public class GraphUtility {
 			graph.addVertex(dest.toString(), dest);
 			graph.addEdge(src.toString(), dest.toString());
 		}
-		
+
 		return graph.breadthFirstSearch(srcData, dstData);
 	}
 
 	public static <Type> List<Type> sort(List<Type> sources, List<Type> destinations) {
+		if (sources == null || destinations == null) {
+			throw new IllegalArgumentException("Source and destination lists cannot be null.");
+		}
+
+		if (sources.size() != destinations.size()) {
+			throw new IllegalArgumentException("Source and destination lists must be the same size.");
+		}
 		Graph<Type> graph = new Graph<Type>();
 		for (int i = 0; i < sources.size(); i++) {
 			Type src = sources.get(i);
@@ -39,7 +61,7 @@ public class GraphUtility {
 			graph.addVertex(dest.toString(), dest);
 			graph.addEdge(src.toString(), dest.toString());
 		}
-		
+
 		return graph.topoSort();
 	}
 }
